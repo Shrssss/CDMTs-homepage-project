d80f90dff97bf742834ffe8387ac75f5bf465326
Shrssss <harunnia764@gmail.com>
2026-06-12 21:13:54 +0900

    add:NewsDTOの追加

--- homepage/src/main/java/net/codemates/homepage/model/dto/news/NewsCreateRequest.java
+++ homepage/src/main/java/net/codemates/homepage/model/dto/news/NewsCreateRequest.java
@@ -1,9 +1,27 @@
 package net.codemates.homepage.model.dto.news;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 public class NewsCreateRequest {
 	
-	/*	報告記事を投稿するためのpObjectを定義するクラス
-	 * 
-	 */
+	/*　報告記事を投稿するためのpObjectを定義するクラス　*/
+	
+	@NotBlank
+	@Size(max=100)
+	private String title;
+	
+	@NotBlank
+	private String content;
+	
+	private String thumbnail_path;
+	
+	@NotBlank
+	@Size(max=30)
+	private String category;
+	
+	@NotNull
+	private Boolean isPublished;
 
 }
