9890d53f693cd6c032da41700e69b25bde592fbd
Shrssss <harunnia764@gmail.com>
2026-06-24 20:29:37 +0900

    change:ItemRentalHistoryの引数をListに変更

--- homepage/src/main/java/net/codemates/homepage/mapper/ItemRentalHistoryMapper.java
+++ homepage/src/main/java/net/codemates/homepage/mapper/ItemRentalHistoryMapper.java
@@ -4,17 +4,18 @@ import java.util.List;
 import java.time.LocalDateTime;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import net.codemates.homepage.model.entity.ItemRentalHistory;
 
 @Mapper
 public interface ItemRentalHistoryMapper {
 	
-	List<ItemRentalHistory> findById(Long id);
+	List<ItemRentalHistory> findByIds(@Param("ids")List<Long> ids);
 	
-	List<ItemRentalHistory> findByItemId(Long itemId);
+	List<ItemRentalHistory> findByItemIds(@Param("itemIds")List<Long> itemIds);
 	
-	List<ItemRentalHistory> findByRenterId(Long renterId);
+	List<ItemRentalHistory> findByRenterIds(@Param("renterIds")List<Long> renterIds);
 	
 	List<ItemRentalHistory> findAll();
 	
