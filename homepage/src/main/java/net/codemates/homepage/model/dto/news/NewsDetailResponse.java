8d1c034813c57f2ef33180f4a5d15b22aa8ab5b1
Shrssss <harunnia764@gmail.com>
2026-06-26 13:46:58 +0900

    fix:変数名の修正

--- homepage/src/main/java/net/codemates/homepage/model/dto/news/NewsDetailResponse.java
+++ homepage/src/main/java/net/codemates/homepage/model/dto/news/NewsDetailResponse.java
@@ -7,7 +7,15 @@ import org.springframework.format.annotation.DateTimeFormat;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class NewsDetailResponse {
 
 /*　記事の詳細を表示するためのObjectを定義するクラス */
@@ -16,7 +24,7 @@ public class NewsDetailResponse {
 	@Size(max=100)
 	private String title;
 	
-	private String thumbnail_path;
+	private String thumbnailPath;
 	
 	@NotBlank
 	@Size(max=30)
