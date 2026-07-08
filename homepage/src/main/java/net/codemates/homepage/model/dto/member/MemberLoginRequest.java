ddca1dfca395322769308be8bf5f606817634a78
Shrssss <harunnia764@gmail.com>
2026-07-08 21:45:37 +0900

    remove:unused library

--- homepage/src/main/java/net/codemates/homepage/model/dto/member/MemberLoginRequest.java
+++ homepage/src/main/java/net/codemates/homepage/model/dto/member/MemberLoginRequest.java
@@ -2,7 +2,6 @@ package net.codemates.homepage.model.dto.member;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import net.codemates.homepage.model.entity.Member;
