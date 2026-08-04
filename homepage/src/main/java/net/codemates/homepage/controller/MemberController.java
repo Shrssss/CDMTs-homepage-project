58de9719b1a2e6d0e655c0c5583222f030a3ae46
Shrssss <harunnia764@gmail.com>
2026-08-04 21:40:22 +0900

    delete:delete memo

--- homepage/src/main/java/net/codemates/homepage/controller/MemberController.java
+++ homepage/src/main/java/net/codemates/homepage/controller/MemberController.java
@@ -7,21 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/members")
 public class MemberController {
 	
-	/*	必要な機能
-	 * 
-	 * 		メンバー登録
-	 * 
-	 * 		メンバー検索
-	 * 			├─ 全体表示
-	 *  		├─ フィルター機能
-	 * 			└─ 文字列検索
-	 * 
-	 * 		メンバー情報
-	 * 			├─　詳細表示
-	 * 			└─ 情報更新
-	 * 
-	 */
-	
     /*
      * メンバー作成    （作らないでいい）
      * 
