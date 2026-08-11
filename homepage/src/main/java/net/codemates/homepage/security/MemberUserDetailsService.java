0152a121189a9f3855b1c52bf4bd7ccc7e97d937
Shrssss <harunnia764@gmail.com>
2026-08-12 00:02:49 +0900

    add:spring security用のサービスを追加

--- homepage/src/main/java/net/codemates/homepage/security/MemberUserDetailsService.java
+++ homepage/src/main/java/net/codemates/homepage/security/MemberUserDetailsService.java
@@ -1,5 +1,27 @@
 package net.codemates.homepage.security;
 
-public class MemberUserDetailsService {
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+import net.codemates.homepage.mapper.MemberMapper;
+import net.codemates.homepage.model.entity.Member;
+
+@Service
+@RequiredArgsConstructor
+public class MemberUserDetailsService implements UserDetailsService{
+	
+	private final MemberMapper mapper;
+	
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException{
+		
+		Member member=mapper.findByStudentIdOrEmial(username);
+		
+		if(member==null) throw new UsernameNotFoundException("Member not found");
+		
+		return new MemberUserDetails(member);
+	}
 	
 }
