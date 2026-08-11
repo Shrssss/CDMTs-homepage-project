c384bef016770c5287f736d30d517312d87c308e
Shrssss <harunnia764@gmail.com>
2026-08-11 23:56:20 +0900

    change:auth関連を移動

--- homepage/src/main/java/net/codemates/homepage/security/MemberUserDetailsService.java
+++ homepage/src/main/java/net/codemates/homepage/security/MemberUserDetailsService.java
@@ -1,5 +1,24 @@
 package net.codemates.homepage.security;
 
 public class MemberUserDetailsService {
-
+	
+//	private final PasswordEncoder passwordEncoder;
+	
+	
+	//未完成
+	//JWT or Spring Session
+	//要検討
+//	public Long login(MemberLoginRequest memberDto) {
+//		
+//		Member memberEntity=mapper.findByStudentIdOrEmial(memberDto.getIdentifier());
+//		
+//		if(memberEntity==null) throw new IllegalArgumentException("Login Failed");
+//		
+//		if(!passwordEncoder.matches(memberDto.getPassword(),memberEntity.getPasswordHash())) {
+//			throw new IllegalArgumentException("Login Failed");
+//		}
+//		
+//		return memberEntity.getId();
+//	}
+	
 }
--- homepage/src/main/java/net/codemates/homepage/service/MemberService.java
+++ homepage/src/main/java/net/codemates/homepage/service/MemberService.java
@@ -1,12 +1,8 @@
 package net.codemates.homepage.service;
 
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
-import net.codemates.homepage.mapper.MemberMapper;
-import net.codemates.homepage.model.dto.member.MemberLoginRequest;
-import net.codemates.homepage.model.entity.Member;
 
 @Service
 @RequiredArgsConstructor
@@ -26,26 +22,5 @@ public class MemberService {
 	 * 			└─ 情報更新
 	 * 
 	 */
-	
-	private final MemberMapper mapper;
-	
-//	private final PasswordEncoder passwordEncoder;
-	
-	
-	//未完成
-	//JWT or Spring Session
-	//要検討
-//	public Long login(MemberLoginRequest memberDto) {
-//		
-//		Member memberEntity=mapper.findByStudentIdOrEmial(memberDto.getIdentifier());
-//		
-//		if(memberEntity==null) throw new IllegalArgumentException("Login Failed");
-//		
-//		if(!passwordEncoder.matches(memberDto.getPassword(),memberEntity.getPasswordHash())) {
-//			throw new IllegalArgumentException("Login Failed");
-//		}
-//		
-//		return memberEntity.getId();
-//	}
 	
 }
