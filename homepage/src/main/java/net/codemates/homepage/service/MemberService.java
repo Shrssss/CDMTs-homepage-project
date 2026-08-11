682a4a874465cdf930b4e5f49b4a53a2b151b882
Shrssss <harunnia764@gmail.com>
2026-08-11 23:57:38 +0900

    reset:直前の変更を破棄

--- homepage/src/main/java/net/codemates/homepage/service/MemberService.java
+++ homepage/src/main/java/net/codemates/homepage/service/MemberService.java
@@ -23,4 +23,19 @@ public class MemberService {
 	 * 
 	 */
 	
+//	private final PasswordEncoder passwordEncoder;
+	
+//	public Long login(MemberLoginRequest memberDto) {
+//		
+//		Member memberEntity=mapper.findByStudentIdOrEmial(memberDto.getIdentifier());
+//		
+//		if(memberEntity==null) throw new IllegalArgumentException("Login Failed");
+//		
+//		if(!passwordEncoder.matches(memberDto.getPassword(),memberEntity.getPasswordHash())) {
+//			throw new IllegalArgumentException("Login Failed");
+//		}
+//		
+//		return memberEntity.getId();
+//	}
+	
 }
