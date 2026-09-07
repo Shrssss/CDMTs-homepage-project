7f31aa26d28c488beb733fdcbd1d86621fd0ce98
Shrssss <harunnia764@gmail.com>
2026-09-07 13:27:00 +0900

    refactor:remove CorsConfig and integrate CORS configuration into SecurityConfig

--- homepage/src/main/java/net/codemates/homepage/config/SecurityConfig.java
+++ homepage/src/main/java/net/codemates/homepage/config/SecurityConfig.java
@@ -1,11 +1,20 @@
 package net.codemates.homepage.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import jakarta.servlet.http.HttpServletResponse;
 
@@ -20,18 +29,59 @@ public class SecurityConfig {
 	}
 	
 	@Bean
+	AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception{
+		
+		return configuration.getAuthenticationManager();
+		
+	}
+	
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		
+		CorsConfiguration configuration=new CorsConfiguration();
+		//別オリジンのURL
+		configuration.setAllowedOrigins(List.of(" !! placeHolder !! "));
+		//許可するHTTPメソッド
+		configuration.setAllowedMethods(List.of(" !! placeHolder !! "));
+		//許可するHTTPヘッダ
+		configuration.setAllowedHeaders(List.of("*"));
+		//Cookie(JSESSIONID)付きリクエストを許可
+		configuration.setAllowCredentials(true);
+		
+		//CORSを適用するURLを設定
+		UrlBasedCorsConfigurationSource source=new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/api/**",configuration);
+		
+		return source;
+		
+	}
+	
+	@Bean
 	SecurityFilterChain securityFilterChain(HttpSecurity http)throws Exception{
 		
-		http.csrf(csrf->csrf.disable()) //CSRFについては後で検討
+		//CSRFについては後で検討
+		http.csrf(csrf->csrf.disable())
+		.cors(cors->cors.configurationSource(corsConfigurationSource()))
+		//ログイン時に必要になったらセッションを作成する
+		.sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED))
 		.authorizeHttpRequests(auth->auth
-				.requestMatchers(" !! placeHolder !! ").permitAll() //ログインや会員登録などの認証不要なエンドポイントを指定
+				 //ログインや会員登録などの認証不要なエンドポイントを指定
+				.requestMatchers(HttpMethod.POST,"/api/auth","/api/auth/login").permitAll()
 				.anyRequest().authenticated()
 				)
-		.formLogin(form->form.disable()) //Spring Securityのデフォルトのログインフォームを無効化(フロントの画面を使用)
+		//Spring Securityのデフォルトのログインフォームを無効化(フロントの画面を使用)
+		.formLogin(form->form.disable()) 
 		.logout(logout->logout
-				.logoutUrl(" !! placeHolder !! ")	//ログアウトエンドポイントを指定
-				.logoutSuccessHandler((request,response,authentication)->{	//ログアウト成功時の処理を定義
-					response.setStatus(HttpServletResponse.SC_OK);	//ログアウト成功時のステータスコードを設定
+				//ログアウトエンドポイントを指定
+				.logoutUrl("/api/auth/logout")
+				//HttpSessionを破棄
+				.invalidateHttpSession(true)
+				//SecurityContextHolderの認証情報をクリア
+				.clearAuthentication(true)
+				//ログアウト成功時の処理を定義
+				.logoutSuccessHandler((request,response,authentication)->{
+					//ログアウト成功時のステータスコードを設定
+					response.setStatus(HttpServletResponse.SC_OK);
 				})
 		);
 		
